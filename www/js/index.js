546c1fe803c363e40dfb549a2b6f487aa70da29e
bg1ufp <bg1ufp@163.com>
2017-07-05 08:48:36 +0800

    Signed-off-by: bg1ufp <bg1ufp@163.com>

--- www/js/index.js
+++ www/js/index.js
@@ -98,7 +98,33 @@ var app = {
             },'json');
 
         });
+        $("#bbarscan").click(function(evt){
+            console.log("scan");
+            cordova.plugins.barcodeScanner.scan(
+                function (result) {
+                    alert("We got a barcode\n" +
+                          "Result: " + result.text + "\n" +
+                          "Format: " + result.format + "\n" +
+                          "Cancelled: " + result.cancelled);
+                },
+                function (error) {
+                    alert("Scanning failed: " + error);
+                },
+                {
+                    //preferFrontCamera : true, // iOS and Android 
+                    //showFlipCameraButton : true, // iOS and Android 
+                    //showTorchButton : true, // iOS and Android 
+                    //torchOn: true, // Android, launch with the torch switched on (if available) 
+                    prompt : "请将要扫描的二维码放入框内", // Android 
+                    resultDisplayDuration: 300, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500 
+                    //formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
+                    //orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device 
+                    //disableAnimations : true, // iOS 
+                    disableSuccessBeep: false // iOS 
+                }
+             );    
 
+        });
         $("#roadselect").change(function(evt){
             var rdid = $(this).children('option:selected').val(); 
             if(rdid>0){
